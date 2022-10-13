3ca5a1de8c216cd58a02737323188a7d0ed12a56
CarolinaPerez1992 <carolinabperez@outlook.com>
2022-10-13 15:03:59 -0300

    checkbox dinamicos

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const container = document.getElementById("container")
-const print = printCard(amazingevents.events, container)
+//const print = printCard(amazingevents.events, container)
 
-function printCard(events, element){
-    for( let event of events ){
-    element.innerHTML += `
+
+function printCard(event){
+    container.innerHTML += `
     <div class="card" style="width: 18rem;">
         <img src="${event.image}" class="card-img-top" alt="${event.name}">
         <div class="card-body">
@@ -14,4 +14,35 @@ function printCard(events, element){
     </div>
     `
 }
-}
+amazingevents.events.forEach(printCard)
+
+//task4
+
+const categories = amazingevents.events.map((event)=> event.category)
+// .reduce((a, b)=> a + b, [])
+
+const category = new Set(categories)
+
+const checkBox = document.getElementById("category")
+category.forEach((category)=>{
+    checkBox.innerHTML += `
+    <div class="form-check form-check-inline">
+        <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="${category}">
+        <label class="form-check-label" for="inlineCheckbox1">${category}</label>
+    </div>
+     `
+})
+// function printCheckBox(event){
+//     checkBox.innerHTML += `
+//     <div class="form-check form-check-inline">
+//         <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="${event.category}">
+//         <label class="form-check-label" for="inlineCheckbox1">${event.category}</label>
+//     </div>
+//     `
+// }
+// amazingevents.events.forEach(printCheckBox)
+
+// let categoryFilter = amazingevents.events.filter((value)=>{
+//     if(value.category === categor)
+// }
+// )
