43ba8be93c88acd428fdeeb3e33070a0a2eeaa3f
CarolinaPerez1992 <carolinabperez@outlook.com>
2022-10-25 09:49:01 -0300

    condicional

--- js/details.js
+++ js/details.js
@@ -1,3 +1,5 @@
+let actualDate = '2022-10-15'
+
 async function getEvent(id){
   try{
   let response = await fetch(`https://mh-amazing.herokuapp.com/amazing/${id}`)
@@ -5,10 +7,10 @@ async function getEvent(id){
   let event = data.event
   console.log(event)
   let detail = document.getElementById("event")
-  console.log(event.date) 
+  console.log(event.date < actualDate) 
   // if(event.date < amazingevents.currentDate){
+ if(event.date < actualDate){
   detail.innerHTML =
-  // if()
 `<div class="card mb-3" style="max-width: 800px; max-height: 400px;">
        <div class="row g-2">
          <div class="col-md-6">
@@ -29,32 +31,33 @@ async function getEvent(id){
        </div>
      </div>
      `
+      }else{
+       detail.innerHTML =
+       `<div class="card mb-3" style="max-width: 800px; max-height: 400px;">
+       <div class="row g-2">
+         <div class="col-md-6">
+           <img src=${event.image} style="max-width: 700px; max-height: 300px" class="p-5" alt="cinema">
+         </div>
+         <div class="col-md-6">
+           <div class="card-body">
+           <h3 class="card-title">${event.name}</h3>
+           <p class="card-text"> ${event.description}</p>
+           <p class="card-text">Date: ${event.date}</p>
+           <p class="card-text">Category: ${event.category}</p>
+           <p class="card-text">Place: ${event.place}</p>
+           <p class="card-text">Capacity: ${event.capacity}</p>
+           <p class="card-text">Estimate: ${event.estimate}</p>
+           <p class="card-text">Price: $${event.price}</p>
+           </div>
+         </div>
+       </div>
+     </div>
+     `
+   }
   }catch (e) {
     console.error(e)
   }
-  //  }else{
-  //      detail.innerHTML =
-  //      `<div class="card mb-3" style="max-width: 800px; max-height: 400px;">
-  //      <div class="row g-2">
-  //        <div class="col-md-6">
-  //          <img src=${variable3.image} class="p-5" alt="cinema">
-  //        </div>
-  //        <div class="col-md-6">
-  //          <div class="card-body">
-  //          <h3 class="card-title">${variable3.name}</h3>
-  //          <p class="card-text"> ${variable3.description}</p>
-  //          <p class="card-text">Date: ${variable3.date}</p>
-  //          <p class="card-text">Category: ${variable3.category}</p>
-  //          <p class="card-text">Place: ${variable3.place}</p>
-  //          <p class="card-text">Capacity: ${variable3.capacity}</p>
-  //          <p class="card-text">Estimate: ${variable3.estimate}</p>
-  //          <p class="card-text">Price: $${variable3.price}</p>
-  //          </div>
-  //        </div>
-  //      </div>
-  //    </div>
-  //    `
-  //  }
+  
 }
 getEvent(location.search.slice(4))
 console.log(location.search)
